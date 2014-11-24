03fcb893e69af3f136563447777e78b37e4ca25a
Boris Bügling <boris@icculus.org>
2014-11-24 17:48:01 +0100

    Build the basics for the Xcode UI

--- Code/ContentTypeSelectionDialog.m
+++ Code/ContentTypeSelectionDialog.m
@@ -10,14 +10,48 @@
 
 @interface ContentTypeSelectionDialog ()
 
+@property (weak) IBOutlet NSTextField *accessTokenTextField;
+@property (weak) IBOutlet NSButton *generateButton;
+@property (weak) IBOutlet NSButton *loginButton;
+@property (weak) IBOutlet NSMenu *spaceSelectionMenu;
+@property (weak) IBOutlet NSMenu *targetSelectionMenu;
+
 @end
+
+#pragma mark -
 
 @implementation ContentTypeSelectionDialog
 
+- (void)closeSheet {
+    [self.window close];
+    [[NSApp keyWindow] endSheet:self.window];
+}
+
+- (instancetype)init {
+    self = [super initWithWindowNibName:NSStringFromClass(self.class)];
+    return self;
+}
+
 - (void)windowDidLoad {
     [super windowDidLoad];
-    
-    // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+}
+
+#pragma mark - Actions
+
+- (IBAction)cancelButtonClicked:(NSButton *)sender {
+    [self closeSheet];
+}
+
+- (IBAction)generateClicked:(NSButton *)sender {
+
+}
+
+- (IBAction)loginClicked:(NSButton*)sender {
+
+}
+
+- (IBAction)obtainAccessTokenClicked:(NSButton*)sender {
+    [[NSWorkspace sharedWorkspace] openURL:[NSURL URLWithString:@"https://www.contentful.com/developers/documentation/content-management-api/http/#getting-started"]];
 }
 
 @end
--- Code/ContentfulPlugin.m
+++ Code/ContentfulPlugin.m
@@ -7,12 +7,14 @@
 //
 
 #import "ContentfulPlugin.h"
+#import "ContentTypeSelectionDialog.h"
 
 static ContentfulPlugin *sharedPlugin;
 
 @interface ContentfulPlugin()
 
 @property (nonatomic, strong, readwrite) NSBundle *bundle;
+@property (nonatomic, strong, readwrite) ContentTypeSelectionDialog* contentTypeSelectionDialog;
 
 @end
 
@@ -36,18 +38,17 @@ static ContentfulPlugin *sharedPlugin;
     return sharedPlugin;
 }
 
+#pragma mark -
+
 - (id)initWithBundle:(NSBundle *)plugin
 {
     if (self = [super init]) {
         self.bundle = plugin;
-        
-        // Create menu items, initialize UI, etc.
 
-        // Sample Menu Item:
-        NSMenuItem *menuItem = [[NSApp mainMenu] itemWithTitle:@"Edit"];
+        NSMenuItem *menuItem = [[NSApp mainMenu] itemWithTitle:@"Product"];
         if (menuItem) {
             [[menuItem submenu] addItem:[NSMenuItem separatorItem]];
-            NSMenuItem *actionMenuItem = [[NSMenuItem alloc] initWithTitle:@"Do Action" action:@selector(doMenuAction) keyEquivalent:@""];
+            NSMenuItem *actionMenuItem = [[NSMenuItem alloc] initWithTitle:@"Generate Model from Contentful..." action:@selector(doMenuAction) keyEquivalent:@""];
             [actionMenuItem setTarget:self];
             [[menuItem submenu] addItem:actionMenuItem];
         }
@@ -55,17 +56,19 @@ static ContentfulPlugin *sharedPlugin;
     return self;
 }
 
-// Sample Action, for menu item:
 - (void)doMenuAction
 {
-    NSAlert *alert = [[NSAlert alloc] init];
-    [alert setMessageText:@"Hello, World"];
-    [alert runModal];
+    self.contentTypeSelectionDialog = [ContentTypeSelectionDialog new];
+    [[NSApp keyWindow] beginSheet:self.contentTypeSelectionDialog.window completionHandler:nil];
 }
 
 - (void)dealloc
 {
     [[NSNotificationCenter defaultCenter] removeObserver:self];
+}
+
+-(BOOL)validateMenuItem:(NSMenuItem *)menuItem {
+    return [NSStringFromClass([NSApp keyWindow].class) isEqualToString:@"IDEWorkspaceWindow"];
 }
 
 @end
