a3de1e214a4e9f5b5812eb82e7c988259a4815ad
Boris Bügling <boris@icculus.org>
2014-11-25 17:36:12 +0100

    Implement selection of space/target and execution of model generator

--- Code/ContentTypeSelectionDialog.m
+++ Code/ContentTypeSelectionDialog.m
@@ -11,6 +11,7 @@
 #import <ContentfulManagementAPI/CMAError.h>
 
 #import "ContentTypeSelectionDialog.h"
+#import "XcodeProjectManipulation.h"
 
 @interface ContentTypeSelectionDialog ()
 
@@ -18,7 +19,12 @@
 @property (strong) CMAClient* client;
 @property (weak) IBOutlet NSButton *generateButton;
 @property (weak) IBOutlet NSButton *loginButton;
+@property (strong) XcodeProjectManipulation* projectManipulation;
+@property (strong) CMASpace* selectedSpace;
+@property (strong) id<PBXTarget> selectedTarget;
+@property (weak) IBOutlet NSButton *spaceSelection;
 @property (weak) IBOutlet NSMenu *spaceSelectionMenu;
+@property (weak) IBOutlet NSButton *targetSelection;
 @property (weak) IBOutlet NSMenu *targetSelectionMenu;
 
 @end
@@ -44,13 +50,58 @@
     [[NSApp keyWindow] endSheet:self.window];
 }
 
+- (void)fillMenuWithSpaces:(NSArray*)spaces {
+    self.spaceSelection.enabled = spaces.count > 0;
+
+    [self.spaceSelectionMenu removeAllItems];
+
+    spaces = [spaces sortedArrayUsingComparator:^NSComparisonResult(CMASpace* space1, CMASpace* space2) {
+        return [space1.name localizedStandardCompare:space2.name];
+    }];
+    self.selectedSpace = spaces[0];
+
+    [spaces enumerateObjectsUsingBlock:^(CMASpace* space, NSUInteger idx, BOOL *stop) {
+        NSMenuItem *menuItem = [[NSMenuItem alloc] initWithTitle:space.name
+                                                          action:@selector(selectSpace:)
+                                                   keyEquivalent:@""];
+        menuItem.representedObject = space;
+        [self.spaceSelectionMenu addItem:menuItem];
+    }];
+}
+
+- (void)fillMenuWithTargets:(NSArray*)targets {
+    self.targetSelection.enabled = targets.count > 0;
+
+    [self.targetSelectionMenu removeAllItems];
+
+    targets = [targets sortedArrayUsingComparator:^NSComparisonResult(id<PBXTarget> t1, id<PBXTarget> t2) {
+        return [[t1 name] localizedStandardCompare:[t2 name]];
+    }];
+    self.selectedTarget = targets[0];
+
+    [targets enumerateObjectsUsingBlock:^(id<PBXTarget> target, NSUInteger idx, BOOL *stop) {
+        NSMenuItem *menuItem = [[NSMenuItem alloc] initWithTitle:target.name
+                                                          action:@selector(selectTarget:)
+                                                   keyEquivalent:@""];
+        menuItem.representedObject = target;
+        [self.targetSelectionMenu addItem:menuItem];
+    }];
+}
+
 - (instancetype)init {
     self = [super initWithWindowNibName:NSStringFromClass(self.class)];
+    if (self) {
+        self.projectManipulation = [XcodeProjectManipulation new];
+    }
     return self;
 }
 
-- (void)windowDidLoad {
-    [super windowDidLoad];
+-(void)selectSpace:(NSMenuItem*)menuItem {
+    self.selectedSpace = menuItem.representedObject;
+}
+
+-(void)selectTarget:(NSMenuItem*)menuItem {
+    self.selectedTarget = menuItem.representedObject;
 }
 
 #pragma mark - Actions
@@ -60,16 +111,45 @@
 }
 
 - (IBAction)generateClicked:(NSButton *)sender {
+    NSString* generatorBinaryPath = [[NSBundle bundleForClass:self.class] pathForResource:@"ContentfulModelGenerator" ofType:nil];
 
+    NSTask* task = [NSTask new];
+    task.arguments = @[@"generate", [@"--spaceKey=" stringByAppendingString:self.selectedSpace.identifier], [@"--accessToken=" stringByAppendingString:self.accessTokenTextField.stringValue]];
+    task.currentDirectoryPath = [self.projectManipulation workspacePath];
+    task.launchPath = generatorBinaryPath;
+
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        [task launch];
+        [task waitUntilExit];
+
+        NSString* potentialPath = [[self.projectManipulation workspacePath] stringByAppendingPathComponent:@"ContentfulModel.xcdatamodeld"];
+        BOOL exists = [[NSFileManager defaultManager] fileExistsAtPath:potentialPath];
+
+        dispatch_sync(dispatch_get_main_queue(), ^{
+            if (!exists) {
+                NSAlert* alert = [NSAlert new];
+                alert.messageText = NSLocalizedString(@"Could not generate data model.", nil);
+                [alert runModal];
+            } else {
+                [self.projectManipulation addFileAtPath:potentialPath toTarget:self.selectedTarget];
+            }
+            
+            [self closeSheet];
+        });
+    });
 }
 
 - (IBAction)loginClicked:(NSButton*)sender {
     self.client = [[CMAClient alloc] initWithAccessToken:self.accessTokenTextField.stringValue];
 
     [self.client fetchAllSpacesWithSuccess:^(CDAResponse *response, CDAArray *array) {
-        NSLog(@"%@", array);
+        [self fillMenuWithSpaces:array.items];
+        [self fillMenuWithTargets:[self.projectManipulation targets]];
+
+        self.generateButton.enabled = self.spaceSelection.enabled && self.targetSelection.enabled;
     } failure:^(CDAResponse *response, NSError *error) {
-        NSLog(@"Error: %@", error);
+        NSAlert* alert = [NSAlert alertWithError:error];
+        [alert runModal];
     }];
 }
 
